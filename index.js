e46800664801504d2f549a1b1f16b75bf49a7608
Eli <elitward@users.noreply.github.com>
2020-08-15 02:28:19 -0700

    the working code to get src attribute from a script tag

--- index.js
+++ index.js
@@ -19,6 +19,29 @@ var c = new Crawler({
             //a lean implementation of core jQuery designed specifically for the server
             console.log($("title").text());
 
+            // var scripts = $("script");
+            // if(Array.isArray(scripts)) {
+            //     scripts.forEach(script => {
+            //         console.log("script", script);
+            //     });
+            // }
+            // if(typeof scripts === 'object'){
+            //     for (var key in scripts) {
+            //         if (scripts.hasOwnProperty(key)) {
+            //           // Do things here
+            //           console.log("script", key, scripts[key]);
+            //         }
+            //     }
+            // }
+
+            var scripts = $("script");
+            scripts.each(function(i, script) {
+                var src = $(script).attr("src");
+                console.log("script: " + src);
+            });
+    
+
+
             var filename = DefaultOutputPath + (res.options.filename ? res.options.filename : "index.html");
             fs.createWriteStream(filename).write(res.body);
         }
